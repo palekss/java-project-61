449a965d6c70955f602a6343b21957e54c1537ab
pas <perepelkin.aleks@gmail.com>
2023-07-07 19:24:28 +0300

    step 5: changed App.java, added Even.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,23 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\nYour choice: ");
+        Scanner scan = new Scanner(System.in);
+        String numberGame = scan.next();
+        if (numberGame.equals("0")) {
+            System.exit(0);
+        } else if (numberGame.equals("1")) {
+            System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+            String name = scan.next();
+            System.out.println("Hello, " + name + "!");
+        } else if (numberGame.equals("2")) {
+            Even.greeting();
+        } else {
+            System.out.println("This game doesn't exist!");
+            System.exit(0);
+        }
     }
 }
