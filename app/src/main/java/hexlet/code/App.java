6b3849792cbe26d87cb12840ec45556d3dfa1b40
pas <perepelkin.aleks@gmail.com>
2023-07-09 18:02:33 +0300

    step 6: changed App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,20 +1,28 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\nYour choice: ");
+        System.out.print("Please enter the game number and press Enter."
+                + "\n1 - Greet"
+                + "\n2 - Even"
+                + "\n3 - Calc"
+                + "\n0 - Exit"
+                + "\nYour choice: ");
         Scanner scan = new Scanner(System.in);
         String numberGame = scan.next();
         if (numberGame.equals("0")) {
             System.exit(0);
         } else if (numberGame.equals("1")) {
-            System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-            String name = scan.next();
-            System.out.println("Hello, " + name + "!");
+            Engine.greeting();
         } else if (numberGame.equals("2")) {
-            Even.greeting();
+            Even.starting();
+        } else if (numberGame.equals("3")) {
+            Calc.starting();
         } else {
             System.out.println("This game doesn't exist!");
             System.exit(0);
