a9dcc24c2589f59de39d286b5864b7a6738aad43
pas <perepelkin.aleks@gmail.com>
2023-07-09 18:01:28 +0300

    step 6: changed and moved Even.java in directory hexlet/code/games/

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,4 +1,6 @@
-package hexlet.code;
+package hexlet.code.games;
+
+import hexlet.code.Engine;
 
 import java.util.Scanner;
 
@@ -10,13 +12,7 @@ public class Even {
 
     static String nameUser;
 
-    static void starting() {
-//        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-//        nameUser = scan.next();
-//        System.out.println("Hello, " + nameUser + "!\nAnswer 'yes' if the number is even, otherwise answer 'no'.");
-//        game();
-//        System.out.println("Congratulations, " + nameUser + "!");
-//        scan.close();
+    public static void starting() {
         nameUser = Engine.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         game();
@@ -27,21 +23,13 @@ public class Even {
         while (i < 3) {
             int question = Engine.getRandomInt(0, 1000);
             String answer = Engine.question(String.valueOf(question));
-//            System.out.print("Question: " + question + "\nYour answer: ");
-//            String answer = scan.next();
             if ((answer.equals("yes") && (question % 2 == 0)) || (answer.equals("no") && (question % 2 != 0))) {
                 System.out.println("Correct!");
                 i++;
             } else {
                 if (question % 2 == 0) {
-//                    System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\n"
-//                            + "Let's try again, " + nameUser + "!");
-//                    System.exit(0);
                     Engine.mistake(answer, "yes", nameUser);
                 } else {
-//                    System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\n"
-//                            + "Let's try again, " + nameUser + "!");
-//                    System.exit(0);
                     Engine.mistake(answer, "no", nameUser);
                 }
             }
