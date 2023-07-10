8b386fd44586c6e455062e5ebae6b9461be45c84
pas <perepelkin.aleks@gmail.com>
2023-07-10 20:46:07 +0300

    step 7: created GSD.java in directory hexlet/code/games/

---
+++ app/src/main/java/hexlet/code/games/GSD.java
@@ -0,0 +1,42 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Scanner;
+public class GSD {
+    static Scanner scan = new Scanner(System.in);
+    static String nameUser;
+
+    public static void starting() {
+        nameUser = Engine.greeting();
+        System.out.println("Find the greatest common divisor of given numbers.");
+        game();
+        Engine.congratulating(nameUser);
+    }
+
+    public static void game() {
+        var i = 0;
+        while (i < 3) {
+            int firstNumber = Engine.getRandomInt(1, 100);
+            int secondNumber = Engine.getRandomInt(1, 100);
+            int firstValue = firstNumber;
+            int secondValue = secondNumber;
+            int result = 0;
+            while (firstValue != 0 && secondValue != 0) {
+                if (firstValue > secondValue) {
+                    firstValue = firstValue % secondValue;
+                } else {
+                    secondValue = secondValue % firstValue;
+                }
+                result = firstValue + secondValue;
+            }
+            String answer = Engine.question(firstNumber + " " + secondNumber);
+            if (answer.equals(String.valueOf(result))) {
+                System.out.println("Correct!");
+                i++;
+            } else {
+                Engine.mistake(answer, String.valueOf(result), nameUser);
+            }
+        }
+    }
+
+}
